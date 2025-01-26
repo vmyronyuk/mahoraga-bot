9c01e0df302cc9dd3ed80b646b3782e88d85ec15
vmyronyuk <vitalikmur38@gmail.com>
2025-01-26 12:33:34 +0200

    [MAH-13] Cache user list

--- src/firebase/vs/getRandomUser.ts
+++ src/firebase/vs/getRandomUser.ts
@@ -1,34 +1,43 @@
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, query, where } from 'firebase/firestore'
 import { UserDTO } from '../../dtos/user.dto'
 import { db } from '../config'
+
+let cachedUsers: UserDTO[] | null = null
 
 export const getRandomUser = async (
 	currentUserId: string
 ): Promise<UserDTO | null> => {
+	if (cachedUsers) {
+		const filteredUsers = (cachedUsers as UserDTO[]).filter(
+			user => user.id !== currentUserId
+		)
+		return getRandomFromArray(filteredUsers)
+	}
+
 	const usersRef = collection(db, 'users')
-	const querySnapshot = await getDocs(usersRef)
+	const querySnapshot = await getDocs(
+		query(usersRef, where('id', '!=', currentUserId))
+	)
 
 	if (querySnapshot.empty) {
 		console.warn('No users found in the database.')
 		return null
 	}
 
-	const users = querySnapshot.docs
-		.map(doc => {
-			const data = doc.data() as UserDTO
-			return {
-				...data,
-				id: doc.id,
-			}
-		})
-		.filter(user => user.id !== currentUserId)
+	cachedUsers = querySnapshot.docs.map(doc => ({
+		...(doc.data() as UserDTO),
+		id: doc.id,
+	}))
 
+	return getRandomFromArray(cachedUsers)
+}
+
+const getRandomFromArray = (users: UserDTO[]): UserDTO | null => {
 	if (users.length === 0) {
-		console.warn('No other users available to choose from.')
+		console.warn('No users available to choose from.')
 		return null
 	}
 
 	const randomUser = users[Math.floor(Math.random() * users.length)]
-
 	return randomUser
 }
